187df3351a71dfb41eada58dc5d3f7852ce56567
moneoasier <moneo.asier@uni.eus>
2021-09-28 08:22:12 +0200

    conexion proiektua

---
+++ Conexion/conexion/src/main/java/eus/uni/dam/ConexionApplication.java
@@ -0,0 +1,13 @@
+package eus.uni.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionApplication.class, args);
+	}
+
+}
