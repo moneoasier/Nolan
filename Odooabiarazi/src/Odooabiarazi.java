71f29e521f43fd471f1432631441c7b372953118
moneoasier <moneo.asier@uni.eus>
2021-10-01 08:34:48 +0200

    zerbitzu eta prozesu

---
+++ Odooabiarazi/src/Odooabiarazi.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Odooabiarazi {
+
+	
+		// TODO Auto-generated method stub
+		public void Odooabiarazi() {
+	
+		
+		}
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		ProcessBuilder pb1=new ProcessBuilder("Firefox.exe", "192.168.65.2:8069");
+		
+		Process p1= pb1.start();
+	}
+
+}
