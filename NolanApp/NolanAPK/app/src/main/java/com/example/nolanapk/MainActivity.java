aa66e71c97e3af51a5586f6e85ea53425b5691c2
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2021-10-07 13:14:18 +0200

    dokumentazioa uwu

--- NolanApp/NolanAPK/app/src/main/java/com/example/nolanapk/MainActivity.java
+++ NolanApp/NolanAPK/app/src/main/java/com/example/nolanapk/MainActivity.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Aldagai globalen deklarazioa
     ImageView b1;
     ImageView b2;
     ImageView b3;
@@ -47,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    //Aplikazioa abiaraztean aldagaiei emandako balioak
         b1 = findViewById(R.id.btn1);
         b2 = findViewById(R.id.btn2);
         b3 = findViewById(R.id.btn3);
@@ -58,20 +59,25 @@ public class MainActivity extends AppCompatActivity {
 
         spin=findViewById(R.id.filter);
 
+        //Metodo hauei deitzen zaie aplikazioa abiaraztean
         readData();
         showData(start,allGafas);
 
 
     }
 
+    //Metodo honek MainActivity2 irekitzen du ImageView bat klikatzean
     public void nextScreen(View v){
+
         Intent b = new Intent(MainActivity.this, MainActivity2.class);
 
         String valor= v.getTag().toString();
+        //Balio bat gordetzen du MainActivity2-ra pasatzeko
         b.putExtra("gafa",findGafa(valor));
         startActivity(b);
     }
 
+    //String bat hartzen du eta ArrayList allGafas-eko elementuekin konparatzen du, balio berdina duen elementua aurkitzean, elementu hori bueltatuko du
     public String findGafa(String name){
         for(Gafa g: allGafas){
             if(g.getId().toLowerCase().equals(name)){
@@ -81,10 +87,14 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    //next edo previous botoiak klikatzean atzera edo aurrera joaten da erregistroen zehar
     @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
     public void btnChange(View v){
         switch (v.getId()) {
             case R.id.next:
+
+                //goToNextPage true bada hurrengo 4 erregistroak erakusten ditu
+                // aukeratutako balioaren arabera betaurreko guztiak edo filtratuak erakusten ditu
                 if (goToNextPage()) {
                     start += 4;
                     if (selection.equals("All")){
@@ -98,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
 
+            //goToPreviousPage true bada aurreko 4 erregistroak erakusten ditu
+            // aukeratutako balioaren arabera betaurreko guztiak edo filtratuak erakusten ditu
             case R.id.back:
                 if (goToPreviousPage()) {
                     start -= 4;
@@ -122,6 +134,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean goToPreviousPage(){
         return currentPage>1;
     }
+
+    //gafas.csv fitxategiaren datuak irakurri eta ArrayList batean gordetzen ditu
     @SuppressLint("SetTextI18n")
     public void readData() {
 
@@ -142,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
         calculatePages(allGafas);
         pages.setText(currentPage +" / "+ totalPages);
     }
+
+    //ArrayList bateko erregistro kopuruaren arabera, zenbat orri izango dituen aplikazioa kalkulatzen du
     public void calculatePages(ArrayList<Gafa> g){
         if(g.size()%4==0){
             totalPages=g.size()/4;
@@ -150,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //zenbaki bat eta erakutsi nahi diren erregistroen ArrayList-a hartzen ditu eta hurrengo edo aurreko 4 erregistroak erakusten ditu
     public void showData(int start,ArrayList<Gafa> g) {
         start--;
         if (start < g.size()) {
@@ -178,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //ImageView, zenbakia eta ArrayList bat hartzen ditu eta irudi bat ipintzen dio ImageView-ra
     public void img(ImageView image, int position,ArrayList<Gafa> g){
         String img_value = "@drawable/"+g.get(position).getId().toLowerCase();
         image.setTag(g.get(position).getId().toLowerCase());
@@ -185,6 +203,8 @@ public class MainActivity extends AppCompatActivity {
         image.setImageResource(img_res);
     }
 
+    //SEARCH botoia klikatzean, aukeratutako filtroaren arabera erregistroak aldatzen ditu
+    @SuppressLint("SetTextI18n")
     public void filterGafas(View v){
         start = 1;
         selection=spin.getSelectedItem().toString();
