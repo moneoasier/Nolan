01c9c2a534f5a578a6f9f0116a7afda4b2cf2fa8
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2021-10-26 13:02:14 +0200

    jpa

---
+++ PruebaSpringSQL/src/main/java/nolan/PruebaSpringSqlApplication.java
@@ -0,0 +1,13 @@
+package nolan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringSqlApplication.class, args);
+	}
+
+}
---
+++ PruebaSpringSQL/src/test/java/nolan/PruebaSpringSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package nolan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
